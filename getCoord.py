ac05db136540920bf295475b24ba9a0124db0320
nooblyh <472897494@qq.com>
2019-06-11 20:55:30 +0800

    get coords

--- getCoord.py
+++ getCoord.py
@@ -5,16 +5,21 @@ import time
 
 file = open('city.txt', mode='r', encoding='UTF-8-sig')
 citys = file.readlines()
+i = 0
 for line in citys:
+    i+=1
     line = line.strip()
     line = line.split(' → ')
     print(line)
+    list = []
 #list = ['江油','平武','剑阁','三台','江油']
     for city in line:
         print(city)
         newurl = 'http://api.map.baidu.com/place/v2/search?query='+city+'&region=Asian&output=json&ak=VEbNXPaYgMqt19eieq6hWKf4jqwZIuUs'
         print(newurl)
         req = requests.post(newurl)
-        time.sleep(5)
+        time.sleep(10)
         req.encoding = 'utf-8'
-        print(json.loads(req.text))
+        list.append(json.loads(req.text))
+    with open('/coords/coord'+str(i)+'.txt','w+') as coord:
+            json.dump(list,coord)
