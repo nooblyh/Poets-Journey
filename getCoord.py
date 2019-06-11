23ecc8c29f2c5c86cfa45496512f3a1c05f81a03
nooblyh <472897494@qq.com>
2019-06-11 16:31:12 +0800

    get coord

---
+++ getCoord.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+import requests
+import json
+import time
+
+file = open('city.txt', mode='r', encoding='UTF-8-sig')
+citys = file.readlines()
+for line in citys:
+    line = line.strip()
+    line = line.split(' → ')
+    print(line)
+#list = ['江油','平武','剑阁','三台','江油']
+    for city in line:
+        print(city)
+        newurl = 'http://api.map.baidu.com/place/v2/search?query='+city+'&region=Asian&output=json&ak=VEbNXPaYgMqt19eieq6hWKf4jqwZIuUs'
+        print(newurl)
+        req = requests.post(newurl)
+        time.sleep(5)
+        req.encoding = 'utf-8'
+        print(json.loads(req.text))
