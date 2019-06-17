55e2c04028873b1d347bd2149d1bb97662a6047e
nooblyh <472897494@qq.com>
2019-06-17 13:30:50 +0800

    collect stages

---
+++ locations/getStage.py
@@ -0,0 +1,14 @@
+import json
+list=[]
+file = open('LiBai.txt', mode='r',encoding='UTF-8-sig')
+lines = file.readlines()
+i = 0
+for line in lines:
+    line.strip()
+    if i % 3 == 0:
+        line = line[:-1]
+        list.append(line)
+    i+=1
+tofile = open('LiBaiStage.txt', mode='w+',encoding='UTF-8-sig')
+tofile.write(str(list))
+tofile.close()
