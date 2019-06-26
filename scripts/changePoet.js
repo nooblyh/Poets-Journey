2b88214c28268abc8370a79dfc08ce868ca0ef09
your name <your email>
2019-06-26 18:00:30 +0800

    change

---
+++ scripts/changePoet.js
@@ -0,0 +1,114 @@
+var poetsInfo = [{
+    "name": "LiBai",
+    "chiName":"李白",
+    "img": "http://www.hwjyw.com/zhwh/traditional_culture/zgwx/gdwx/200712/W020071213534008368140.jpg",
+    "intro":"李白（701年－762年） ，字太白，号青莲居士，又号“谪仙人”，是唐代伟大的浪漫主义诗人，被后人誉为“诗仙”"
+}, {
+    "name": "DuFu",
+    "chiName":"杜甫",
+    "img": "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3579140198,216816410&fm=26&gp=0.jpg",
+    "intro":"杜甫（712年—770年），字子美，自号少陵野老，唐代伟大的现实主义诗人，与李白合称“李杜”。\
+    杜甫在中国古典诗歌中的影响非常深远，被后人称为“诗圣”，他的诗被称为“诗史”。"
+}, {
+    "name": "MengHaoRan",
+    "chiName":"孟浩然",
+    "img": "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3470644387,3551881596&fm=26&gp=0.jpg",
+    "intro":"孟浩然（689年—740年），名浩，字浩然，号孟山人，\
+    襄州襄阳（今湖北襄阳）人，唐代著名的山水田园派诗人，世称“孟襄阳”。因他未曾入仕，又称之为“孟山人”。",
+}, {
+    "name": "WangWei",
+    "chiName":"王维",
+    "img": "../images/WangWei.jpg",
+    "intro":"王维（701年－761年，一说699年—761年），字摩诘，号摩诘居士。\
+    王维参禅悟理，学庄信道，精通诗、书、画、音乐等，以诗名盛于开元、天宝间，尤长五言，多咏山水田园，与孟浩然合称“王孟”，有“诗佛”之称"
+},
+{
+    "name": "BaoZheng",
+    "chiName":"包拯",
+    "img": "../images/BaoZheng.jpg",
+    "intro":"包拯（999年－1062年7月3日），字希仁。庐州合肥（今安徽合肥肥东）人。北宋名臣。"
+},
+{
+    "name": "CenCan",
+    "chiName":"岑参",
+    "img": "../images/CenShen.jpg",
+    "intro":"岑参（约715年—约770年），荆州江陵（有争议）人，唐代诗人，被誉为边塞派诗人，与高适并称“高岑”。"
+},
+{
+    "name": "ChengYi",
+    "chiName":"程颐",
+    "img": "../images/ChengYi.jpg",
+    "intro":"程颐（1033年9月11日—1107年10月5日） [1]  ，字正叔，世居中山，后徙为河南府洛阳（今河南洛阳） 人，\
+    世称伊川先生，北宋理学家、教育家。为程颢之胞弟。历官汝州团练推官、西京国子监教授。元祐元年（1086年）除秘书省校书郎，授崇政殿说书。"
+},
+{
+    "name": "GaoShi",
+    "chiName":"高适",
+    "img": "../images/GaoShi.jpg",
+    "intro":"高适（704—765年），字达夫，作为著名边塞诗人，高适与岑参并称“高岑”，与岑参、王昌龄、王之涣合称“边塞四诗人”。其诗笔力雄健，气势奔放，洋溢着盛唐时期所特有的奋发进取、蓬勃向上的时代精神。"
+},
+{
+    "name": "HanYu",
+    "chiName":"韩愈",
+    "img": "../images/HanYu.jpg",
+    "intro":"韩愈（768年－824年12月25日），字退之，是唐代古文运动的倡导者，被后人尊为“唐宋八大家”之首，与柳宗元并称“韩柳”，有“文章巨公”和“百代文宗”之名。后人将其与柳宗元、欧阳修和苏轼合称“千古文章四大家”。"
+},
+{
+    "name": "HuangTingJian",
+    "chiName":"黄庭坚",
+    "img": "../images/HuangTingJian.jpg",
+    "intro":"黄庭坚（1045年8月9日—1105年5月24日），字鲁直，号山谷道人、涪翁，洪州分宁（江西省九江市修水县）人，北宋著名文学家、书法家、江西诗派开山之祖。"
+},
+{
+    "name": "JiaDao",
+    "chiName":"贾岛",
+    "img": "../images/JiaDao.jpg",
+    "intro":"贾岛（779～843年），唐代诗人，字阆（读láng）仙，人称“诗奴”，与孟郊共称“郊寒岛瘦”，汉族，唐朝河北道幽州范阳县（今河北省涿州）人。自号“碣石山人”。"
+},
+{
+    "name": "KouZhun",
+    "chiName":"寇凖",
+    "img": "../images/KouZhun.jpg",
+    "intro":"寇凖（961年—1023年10月24日），字平仲，华州下邽（今陕西渭南）人。北宋政治家、诗人"
+},
+{
+    "name": "LiDeYu",
+    "chiName":"李德裕",
+    "img": "../images/LiDeYu.jpg",
+    "intro":"李德裕（787年—850年），字文饶，小字台郎，赵郡赞皇（今河北赞皇县）人。唐代杰出政治家、文学家、战略家，中书侍郎李吉甫次子。"
+},
+{
+    "name": "LiHe",
+    "chiName":"李贺",
+    "img": "../images/LiHe.jpg",
+    "intro":"李贺（约公元790年-约817年），字长吉，是“长吉体诗歌开创者。”唐代河南福昌（今河南洛阳宜阳县）人，家居福昌昌谷，后世称李昌谷，是唐宗室，唐高祖李渊的叔父李亮（大郑王）后裔。有“诗鬼”之称"
+},
+{
+    "name": "",
+    "chiName":"",
+    "img": "../images/.jpg",
+    "intro":""
+},{
+    "name": "",
+    "chiName":"",
+    "img": "../images/.jpg",
+    "intro":""
+},{
+    "name": "",
+    "chiName":"",
+    "img": "../images/.jpg",
+    "intro":""
+},{
+    "name": "",
+    "chiName":"",
+    "img": "../images/.jpg",
+    "intro":""
+}]
+
+for(var i = 0;i<poetsInfo.length;i++){
+    if(poetsInfo[i]["name"] == poetName){
+        document.getElementById("chiName").innerHTML = poetsInfo[i]["chiName"];
+        document.getElementById("poetImg").src = poetsInfo[i]["img"];
+        document.getElementById("poetIntro").innerHTML = poetsInfo[i]["intro"];
+    }
+}
